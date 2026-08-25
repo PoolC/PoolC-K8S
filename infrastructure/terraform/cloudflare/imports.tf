eea5a09fefe743c8caaf6e14ac7b512b8c12ea8b
Mayne0213 <bluemayne0213@icloud.com>
2026-08-25 22:54:39 +0900

    CHORE(dns): remove completed imports
    - Remove one-time import blocks after DNS records entered state
    
    - Keep the existing wildcard import block for its prior workflow
    
    - Verified targeted DNS Terraform plan reports no changes

--- infrastructure/terraform/cloudflare/imports.tf
+++ infrastructure/terraform/cloudflare/imports.tf
@@ -2,18 +2,3 @@ import {
   to = cloudflare_dns_record.wildcard_dev_poolc_org
   id = "7d4e80f1a45913c5ece5fe89446b9978/d2104afbf6d417cf10607256f3d7af5a"
 }
-
-import {
-  to = cloudflare_dns_record.git_poolc_org
-  id = "7d4e80f1a45913c5ece5fe89446b9978/7675fc299f2d902baf280729213b8a55"
-}
-
-import {
-  to = cloudflare_dns_record.argocd_poolc_org
-  id = "7d4e80f1a45913c5ece5fe89446b9978/d2362f0338f846eb0b22b1b2601392a8"
-}
-
-import {
-  to = cloudflare_dns_record.grafana_poolc_org
-  id = "7d4e80f1a45913c5ece5fe89446b9978/cb8bf1a7192ed237385f040e5c2edccb"
-}
